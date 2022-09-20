7dd2d583d101785d299b269b66b2ace02dae6a18
Rokas <rookas.rudzenskas@gmail.com>
2022-09-20 09:23:37 +0300

    Rokas

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,18 @@
+import { ChainId, ThirdwebProvider } from '@thirdweb-dev/react'
 import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import { ThemeProvider } from 'next-themes'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+// @ts-ignore
+const MyApp = ({ Component, pageProps }) => {
+  return (
+      <ThirdwebProvider desiredChainId={ChainId.Rinkeby} chainRpc={{
+          [ChainId.Rinkeby]: 'https://rinkeby.infura.io/v3/2f8011c7e4a34befb6d9c0cd2f7f2636'
+      }}>
+        <ThemeProvider enableSystem={true} attribute='class'>
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </ThirdwebProvider>
+  )
 }
 
-export default MyApp
+export default MyApp;
