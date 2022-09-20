9d695156a7911b3cf461d5e973f2fbd9a1735d54
Rokas <rookas.rudzenskas@gmail.com>
2022-09-20 09:26:40 +0300

    Rokas

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,32 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import { useAddress, useMetamask } from '@thirdweb-dev/react'
+
+const style = {
+    wrapper: `flex h-screen items-center justify-center`,
+    connectWalletButton: `rounded-lg border border-black px-10 py-5 transition-all hover:bg-black hover:text-white dark:border-white dark:hover:bg-white dark:hover:text-black`,
+}
 
 const Home: NextPage = () => {
-  return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+    const connectWithMetamask = useMetamask()
+    const address = useAddress()
+
+    return (
+    <div className="">
       <Head>
         <title>OpenSea App 2.2</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
+        <div className={style.wrapper}>
+            <button
+                onClick={connectWithMetamask}
+                className={style.connectWalletButton}
+            >
+                Connect Metamask
+            </button>
+        </div>
 
     </div>
   )
